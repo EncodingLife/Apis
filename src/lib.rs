ed4536033dbaa1b40bc6e96d1501be67f22e5079
Encoding Life <encodinglivingsystems@gmail.com>
2024-08-05 18:19:31 +0200

    Splitting bevy into module, adding prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ mod hex_coord;
 mod hex_trait;
 mod index;
 mod orientation;
-mod shape;
 mod world;
+
+#[cfg(feature = "bevy")]
+pub mod bevy;
 
 pub use crate::edge::Edge;
 pub use crate::flat_map::*;
@@ -14,23 +16,20 @@ pub use crate::hex_coord::HexCoord;
 pub use crate::hex_trait::HexCoordinate;
 pub use crate::index::map_index::MapIndex;
 pub use crate::orientation::HexOrientation;
-#[cfg(feature = "bevy")]
-pub use crate::shape::*;
 pub use crate::world::{HexWorld, HexWorldShape};
 
+#[cfg(feature = "bevy")]
+pub use crate::bevy::*;
+#[cfg(not(feature = "bevy"))]
 pub use glam::{Vec2, Vec3};
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod prelude {
+    pub use crate::{
+        edge::Edge,
+        hex_coord::HexCoord,
+        hex_trait::HexCoordinate,
+        index::map_index::MapIndex,
+        orientation::HexOrientation,
+        world::{HexWorld, HexWorldShape}
+    };
 }
